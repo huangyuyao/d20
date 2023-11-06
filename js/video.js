eb03665e2793e44a2b18eac4c05b3fef01c7d9e6
Yuyao Huang <106633493+huangyuyao@users.noreply.github.com>
2023-11-06 00:17:56 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -8,7 +8,7 @@ window.addEventListener("load", function() {
 // Assume global variable 'video' is defined
 window.addEventListener("load", function() {
 	console.log("Good job opening the window");
-	video = document.querySelector("#player1");
+	video = document.querySelector("video");
 	video.autoplay = false;
 	video.loop = false;
 });
